55b570b896931eab864e3688bc9a6864d2f31862
Ji <irving.j21@googlemail.com>
2020-06-13 11:02:52 +0100

    remove: recipe backend

--- server/router.js
+++ server/router.js
@@ -5,7 +5,7 @@ const ctlrsRec = require('./controllers/recipes');
 const ctlrsUsr = require('./controllers/users');
 
 /* RECIPES */
-router.get('/recipes', ctlrsRec.getRecipes);
+// router.get('/recipes', ctlrsRec.getRecipes);
 // router.get('/randoms', ctlrsRec.getRandom);
 
 /* USER CREATION & VALIDATION */
@@ -14,7 +14,7 @@ router.post('/userLogin', ctlrsUsr.userLogin);
 
 /* MANAGING FAVOURITES */
 router.post('/favourite', ctlrsUsr.addFavourite);
-router.get('/favourites', ctlrsUsr.getFavourites);
+// router.get('/favourites', ctlrsUsr.getFavourites);
 router.get('/favouriteIDs', ctlrsUsr.getFavouritesIDs);
 router.delete('/favourite', ctlrsUsr.deleteFavourite);
 
